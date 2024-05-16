9c95dc58fcf91e0ca99610471fc3e7e7ca8ab9ed
alek.nill@yandex.ru <alek.nill@yandex.ru>
2024-05-16 02:28:11 -0700

    Done

---
+++ LanguageFeatures.Chapter4/Models/MyAsyncMethods.cs
@@ -0,0 +1,11 @@
+﻿namespace LanguageFeatures.Chapter4.Models;
+
+public class MyAsyncMethods
+{
+    public async static Task<long?> GetPageLehgth()
+    {
+        HttpClient client = new HttpClient();
+        var httpTask = await client.GetAsync("http://example.com");
+        return httpTask.Content.Headers.ContentLength;
+    }
+}
